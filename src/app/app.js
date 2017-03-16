16396acf9ccfb8bca309fe1c31f4369b6efed3ee
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-15 21:09:54 -1000

    Removed AppCtrl

--- src/app/app.js
+++ src/app/app.js
@@ -5,28 +5,19 @@ import '../style/app.css'
 let app = () => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
     controllerAs: 'app'
-  }
-}
-
-class AppCtrl {
-  constructor () {
-    this.url = 'https://github.com/preboot/angular-webpack'
   }
 }
 
 const MODULE_NAME = 'app'
 
 angular.module(MODULE_NAME, [])
-  .config(($stateProvider) => {
-    $stateProvider
-       .state(DefaultState.name, DefaultState)
-  })
-  .run(($state) => {
-    $state.go('default')
-  })
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl)
+  // .config(($stateProvider) => {
+  //   $stateProvider
+  //      .state(DefaultState.name, DefaultState)
+  // })
+  // .run(($state) => {
+  //   $state.go('default')
+  // })
 
 export default MODULE_NAME
