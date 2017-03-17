aae315fd0ef78983cc558b837ab63e5e11701df4
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-16 18:20:23 -1000

    added programmerhumor state.js

---
+++ src/app/programmerhumor/state.js
@@ -0,0 +1,9 @@
+const template = require('./template.html')
+
+export const AwwState = {
+  name: 'programmerhumor',
+  url: '/programmerhumor',
+  template,
+  controller: 'PgHumorCtrl',
+  controllerAs: 'programmerhumor'
+}
